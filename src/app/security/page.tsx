6a8bfd271cc64f324bd9cb9e23e191c0dc7bc3a7
Aditya Raj <vickyadityaraj85@gmail.com>
2026-03-29 21:17:55 +0530

    Scanner fixed

--- src/app/security/page.tsx
+++ src/app/security/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useCallback, useState } from 'react'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
